3ac94b08d402048973339a4961802ee4696679e1
jt <jt@localhost>
2018-10-16 08:29:31 -0400

    directory clean up

--- todo/test/t1.test.js
+++
@@ -1,9 +0,0 @@
-const WorkProgram = require('../models/WorkProgram');
-const TodoProject = require('../todo_src/TodoProject');
-
-console.dir(TodoProject);
-
-test("test1" , () => {
-  return undefined;
-
-})
